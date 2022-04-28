bcbcebb01fbbe5ef4bc25754904563933d57e20c
jhhong0509 <jhhong0509@naver.com>
2022-04-29 08:42:41 +0900

    chore: add tbl_outbox

--- authentication-infrastructure/src/main/resources/schema.sql
+++ authentication-infrastructure/src/main/resources/schema.sql
@@ -12,11 +12,19 @@ CREATE TABLE IF NOT EXISTS `tbl_access_management_user` (
     `user_id` BINARY(16) NOT NULL,
     `authority_id` BINARY(16) NOT NULL,
     `version` INT NOT NULL,
-    `state` VARCHAR(20) NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `FK_tbl_authority_TO_tbl_access_management_user_1` FOREIGN KEY (`authority_id`) REFERENCES `tbl_authority`(`id`),
     CONSTRAINT `UNIQUE_user_id_AND_authority_id` UNIQUE (`user_id`, `authority_id`)
 );
+
+CREATE TABLE IF NOT EXISTS `tbl_outbox` (
+    `id` BINARY(16) NOT NULL,
+    `aggregate_id` BINARY(16) NOT NULL,
+    `aggregate_type` VARCHAR(255) NOT NULL,
+    `timestamp` TIMESTAMP NOT NULL DEFAULT NOW(),
+    `type` VARCHAR(255) NOT NULL,
+    `payload` JSON NOT NULL
+)
 
 -- DROP INDEX `access_management_user_index`
 -- CREATE INDEX `access_management_user_index` ON `tbl_access_management_user` (`user_id`);
