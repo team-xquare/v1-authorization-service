57a3b13816398929bea5631e901725924b35ffbd
jhhong0509 <jhhong0509@naver.com>
2022-04-29 08:44:32 +0900

    chore: add dependencies

--- authentication-infrastructure/build.gradle.kts
+++ authentication-infrastructure/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
 }
 
+dependencyManagement {
+    imports {
+        mavenBom(Dependencies.SPRING_CLOUD)
+    }
+}
+
 dependencies {
     implementation(Dependencies.ACTUATOR)
     implementation(Dependencies.WEBFLUX)
@@ -11,11 +17,18 @@ dependencies {
     implementation(Dependencies.REACTIVE_MYSQL)
     implementation(Dependencies.KOTLIN_JACKSON)
     implementation(Dependencies.COROUTINE_REACTOR)
+    implementation(Dependencies.COROUTINE_JDK)
+    implementation(Dependencies.KOTLINX_COROUTINE)
     implementation(Dependencies.COROUTINE_REACTOR_EXTENSION)
     implementation(Dependencies.KOTLIN_STDLIB)
     implementation(Dependencies.KOTLIN_REFLECT)
     implementation(Dependencies.SPRING_VALIDATION)
     implementation(Dependencies.MAPSTRUCT)
+    implementation(Dependencies.SPRING_KAFKA)
+    implementation(Dependencies.REACTOR_KAFKA)
+    implementation(Dependencies.STARTER_SLEUTH)
+    implementation(Dependencies.ZIPKIN)
+    implementation(Dependencies.SPRING_RABBIT)
     kapt(Dependencies.MAPSTRUCT_APT)
     implementation(Dependencies.MICROMETER)
     kapt(Dependencies.CONFIGURATION_PROCESSOR)
