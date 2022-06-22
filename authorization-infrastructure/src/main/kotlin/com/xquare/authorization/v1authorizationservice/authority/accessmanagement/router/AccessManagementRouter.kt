71f66cba39c536db36d08b39d8260b32a0f2a8cb
jhhong0509 <jhhong0509@naver.com>
2022-06-22 10:40:32 +0900

    feat: modify router / handler to sync

---
+++ authorization-infrastructure/src/main/kotlin/com/xquare/authorization/v1authorizationservice/authority/accessmanagement/router/AccessManagementRouter.kt
@@ -0,0 +1,18 @@
+package com.xquare.authorization.v1authorizationservice.authority.accessmanagement.router
+
+import com.xquare.authorization.v1authorizationservice.authority.accessmanagement.handler.UserAuthorityHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+class AccessManagementRouter(
+    private val userAuthorityHandler: UserAuthorityHandler
+) {
+    @Bean
+    fun saveBaseAuthority() = coRouter {
+        "/authorities/access-management".nest {
+            POST("", userAuthorityHandler::createUserBaseAuthority)
+        }
+    }
+}
