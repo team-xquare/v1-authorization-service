d058258a5068c2c0e35063da2408cc7a8507d4e2
jhhong0509 <jhhong0509@naver.com>
2022-06-22 10:38:55 +0900

    feat: delete outbox

--- authorization-infrastructure/src/main/kotlin/com/xquare/authorization/v1authorizationservice/outbox/repository/OutboxRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.xquare.authorization.v1authorizationservice.outbox.repository
-
-import com.xquare.authorization.v1authorizationservice.outbox.OutboxEntity
-import java.util.UUID
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-
-interface OutboxRepository : ReactiveCrudRepository<OutboxEntity, UUID>
