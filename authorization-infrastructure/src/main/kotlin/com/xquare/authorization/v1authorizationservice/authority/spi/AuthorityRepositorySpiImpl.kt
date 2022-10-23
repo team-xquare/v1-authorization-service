c0f623e805d353e6953d93742d3135912db27ec6
홍정현 <57659252+jhhong0509@users.noreply.github.com>
2022-10-23 10:05:43 +0900

    fix: 이름이 영어라 깨지는 문제 해결 (#22)

--- authorization-infrastructure/src/main/kotlin/com/xquare/authorization/v1authorizationservice/authority/spi/AuthorityRepositorySpiImpl.kt
+++ authorization-infrastructure/src/main/kotlin/com/xquare/authorization/v1authorizationservice/authority/spi/AuthorityRepositorySpiImpl.kt
@@ -25,7 +25,7 @@ class AuthorityRepositorySpiImpl(
     private val accessManagementRepository: UserAccessManagementRepository
 ) : AuthorityRepositorySpi {
     companion object {
-        private val DEFAULT_NAME_LIST = listOf("학생")
+        private val DEFAULT_NAME_LIST = listOf("student")
     }
 
     override suspend fun getBaseUserAuthorities(): List<Authority> {
