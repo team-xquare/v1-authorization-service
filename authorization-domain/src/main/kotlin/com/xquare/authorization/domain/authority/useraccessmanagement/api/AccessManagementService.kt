079f9f9e7d3c356b058fc5cac6e89999afcc735c
KimSeungJin <klli1naver@gmail.com>
2023-01-01 13:36:00 +0900

    📑 :: getUserAuthorityListByType 추가

--- authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/useraccessmanagement/api/AccessManagementService.kt
+++ authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/useraccessmanagement/api/AccessManagementService.kt
@@ -1,10 +1,13 @@
 package com.xquare.authorization.domain.authority.useraccessmanagement.api
 
+import com.xquare.authorization.domain.authority.useraccessmanagement.api.dtos.AuthorityListByTypeResponse
 import com.xquare.authorization.domain.authority.useraccessmanagement.api.dtos.AuthorityListResponse
 import java.util.UUID
 
 interface AccessManagementService {
     suspend fun getUserAuthorityList(userId: UUID): AuthorityListResponse
+
+    suspend fun getUserAuthorityListByType(userId: UUID, type: String): AuthorityListByTypeResponse
     suspend fun saveBaseAccessManagement(userId: UUID)
     suspend fun deleteBaseAccessManagement(userId: UUID)
 }
