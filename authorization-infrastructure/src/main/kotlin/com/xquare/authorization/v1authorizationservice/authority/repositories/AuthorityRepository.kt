91fc315aafec7a418a2060ab8d07780b4dcb3f5b
KimSeungJin <klli1naver@gmail.com>
2023-01-01 17:23:39 +0900

    📑 :: findAllByUserIdAndType

--- authorization-infrastructure/src/main/kotlin/com/xquare/authorization/v1authorizationservice/authority/repositories/AuthorityRepository.kt
+++ authorization-infrastructure/src/main/kotlin/com/xquare/authorization/v1authorizationservice/authority/repositories/AuthorityRepository.kt
@@ -1,10 +1,10 @@
 package com.xquare.authorization.v1authorizationservice.authority.repositories
 
 import com.xquare.authorization.v1authorizationservice.authority.AuthorityEntity
-import java.util.UUID
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.data.repository.query.Param
+import java.util.*
 
 interface AuthorityRepository : CoroutineCrudRepository<AuthorityEntity, UUID> {
     suspend fun findAllByNameIn(names: List<String>): List<AuthorityEntity>
@@ -19,4 +19,16 @@ interface AuthorityRepository : CoroutineCrudRepository<AuthorityEntity, UUID> {
         """
     )
     suspend fun findAllByUserId(@Param("userId") userId: UUID): List<AuthorityEntity>
+
+    @Query(
+        """
+        select tbl_authority.id, tbl_authority.name, tbl_authority.description
+        from tbl_authority
+        inner join tbl_access_management_user
+            on tbl_access_management_user.authority_id = tbl_authority.id
+        where tbl_access_management_user.user_id = :userId AND tbl_authority.type = :type
+        """
+    )
+    suspend fun findAllByUserIdAndType(
+        @Param("userId") userId: UUID, @Param("type") type: String): List<AuthorityEntity>
 }
