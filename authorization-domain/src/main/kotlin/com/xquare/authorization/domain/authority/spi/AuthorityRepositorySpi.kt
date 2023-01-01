565626a5686cef0ed77c9d7404ff9be7484b87b5
KimSeungJin <klli1naver@gmail.com>
2023-01-01 13:35:51 +0900

    📑 :: getUserAuthorityListByType 추가

--- authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/spi/AuthorityRepositorySpi.kt
+++ authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/spi/AuthorityRepositorySpi.kt
@@ -2,11 +2,13 @@ package com.xquare.authorization.domain.authority.spi
 
 import com.xquare.authorization.domain.authority.Authority
 import com.xquare.authorization.domain.authority.useraccessmanagement.UserAccessManagement
-import java.util.UUID
+import java.util.*
 
 interface AuthorityRepositorySpi {
     suspend fun getBaseUserAuthorities(): List<Authority>
     suspend fun getUserAuthority(userId: UUID): List<Authority>
+
+    suspend fun getUserAuthorityByType(userId: UUID, type: String): List<Authority>
     suspend fun saveAllUserAccessManagement(userAccessManagements: List<UserAccessManagement>): List<UserAccessManagement>
     suspend fun deleteAllUserAccessManagement(userId: UUID, userAccessManagements: List<UUID>)
 }
