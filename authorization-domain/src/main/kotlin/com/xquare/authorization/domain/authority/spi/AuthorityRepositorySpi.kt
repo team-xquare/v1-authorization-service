c31db61801b3a1fdf8ae449160159ebfc0899e46
KimSeungJin <klli1naver@gmail.com>
2023-01-02 09:47:28 +0900

    ♻️ :: 줄 삭제

--- authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/spi/AuthorityRepositorySpi.kt
+++ authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/spi/AuthorityRepositorySpi.kt
@@ -7,7 +7,6 @@ import java.util.*
 interface AuthorityRepositorySpi {
     suspend fun getBaseUserAuthorities(): List<Authority>
     suspend fun getUserAuthority(userId: UUID): List<Authority>
-
     suspend fun getUserAuthorityByType(userId: UUID, type: String): List<Authority>
     suspend fun saveAllUserAccessManagement(userAccessManagements: List<UserAccessManagement>): List<UserAccessManagement>
     suspend fun deleteAllUserAccessManagement(userId: UUID, userAccessManagements: List<UUID>)
--- authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/useraccessmanagement/api/AccessManagementService.kt
+++ authorization-domain/src/main/kotlin/com/xquare/authorization/domain/authority/useraccessmanagement/api/AccessManagementService.kt
@@ -6,7 +6,6 @@ import java.util.UUID
 
 interface AccessManagementService {
     suspend fun getUserAuthorityList(userId: UUID): AuthorityListResponse
-
     suspend fun getUserAuthorityListByType(userId: UUID, type: String): AuthorityListByTypeResponse
     suspend fun saveBaseAccessManagement(userId: UUID)
     suspend fun deleteBaseAccessManagement(userId: UUID)
