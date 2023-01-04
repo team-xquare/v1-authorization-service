b2263a4427e4c8b882560fcf18ca4fb7611cda46
정대현 <eogus_2513@dsm.hs.kr>
2023-01-04 22:15:27 +0900

    🔀 :: version 삭제

--- authorization-infrastructure/src/main/resources/schema.sql
+++ authorization-infrastructure/src/main/resources/schema.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS `tbl_authority` (
     `id` BINARY(16) NOT NULL,
     `name` VARCHAR(20) NOT NULL,
     `description` VARCHAR(255) NOT NULL,
-    `version` INT NOT NULL,
     `type` VARCHAR(10) NOT NULL,
     PRIMARY KEY (`id`)
 );
@@ -11,7 +10,6 @@ CREATE TABLE IF NOT EXISTS `tbl_access_management_user` (
     `id` BINARY(16) NOT NULL,
     `user_id` BINARY(16) NOT NULL,
     `authority_id` BINARY(16) NOT NULL,
-    `version` INT NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `FK_tbl_authority_TO_tbl_access_management_user_1` FOREIGN KEY (`authority_id`) REFERENCES `tbl_authority`(`id`),
     CONSTRAINT `UNIQUE_user_id_AND_authority_id` UNIQUE (`user_id`, `authority_id`)
